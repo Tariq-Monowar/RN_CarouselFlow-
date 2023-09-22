0c3181a9638f93a6acf2a7fed2738f55b97c0f1f
Tariq-Monowar <101199109+Tariq-Monowar@users.noreply.github.com>
2023-09-22 03:35:37 -0700

    imageData.js

---
+++ imageData.js
@@ -0,0 +1,9 @@
+export const data = [
+    "https://images.pexels.com/photos/18300040/pexels-photo-18300040/free-photo-of-decorative-lantern-with-handle.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3207369/pexels-photo-3207369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/4056110/pexels-photo-4056110.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1726770/pexels-photo-1726770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/18424081/pexels-photo-18424081/free-photo-of-light-sunset-fashion-people.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3540375/pexels-photo-3540375.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
+  
